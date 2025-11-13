cc236c33e7871b97a0e177591ebef800d66c14db
Talha Mirza <talharmirza@gmail.com>
2025-11-13 13:49:05 -0500

    Displayed an element

--- app.js
+++ app.js
@@ -9,7 +9,25 @@ const fetchData = async () => {
         }
 
         const data = await response.json();
-        console.log(data);
+        result = data.result;
+
+        
+        const gallery = document.getElementById("gallery");
+        
+        const card = document.createElement("div");
+        card.className = "card";
+
+        const title = document.createElement("h3");
+        title.className = "title";
+        title.textContent = result[0].properties.title;
+
+        const description = document.createElement("p");
+        description.className = "description";
+        description.textContent = result[0].properties.opening_crawl;
+
+        card.appendChild(title);
+        card.appendChild(description);
+        gallery.appendChild(card);
 
     } catch(err) {
         console.error(err);
