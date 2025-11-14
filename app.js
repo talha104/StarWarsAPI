52c73ef6fded11409dc6666c9190088c2d0b10eb
Talha Mirza <talharmirza@gmail.com>
2025-11-13 23:48:25 -0500

    Both Endpoints Displaying

--- app.js
+++ app.js
@@ -1,37 +1,77 @@
-const fetchData = async () => {
+const filmsButton = document.getElementById("films");
+const peopleButton = document.getElementById("people");
+const gallery = document.getElementById("gallery");
+
+const setActive = activeButton => {
+    filmsButton.classList.remove("active");
+    peopleButton.classList.remove("active");
+    activeButton.classList.add("active");
+} 
+
+
+const fetchData = async (url) => {
 
     try {
 
-        const response = await fetch("https://www.swapi.tech/api/films");
+        const response = await fetch(url);
 
         if (!response.ok) {
             throw new Error("Films data not found");
         }
 
         const data = await response.json();
-        result = data.result;
-
         
-        const gallery = document.getElementById("gallery");
+        gallery.innerHTML = "";
 
+        if (filmsButton.classList.contains("active")) {
 
-        result.forEach(item => {
-            const card = document.createElement("div");
-            card.className = "card";
+            result = data.result;
 
-            const title = document.createElement("h3");
-            title.className = "title";
-            title.textContent = item.properties.title;
+            result.forEach(item => {
 
-            const description = document.createElement("p");
-            description.className = "description";
-            description.textContent = item.properties.opening_crawl;
+                const card = document.createElement("div");
+                card.className = "card";
 
-            card.appendChild(title);
-            card.appendChild(description);
-            gallery.appendChild(card);
-        })
-        
+                const title = document.createElement("h3");
+                title.className = "title";
+                title.textContent = item.properties.title;
+
+                const description = document.createElement("p");
+                description.className = "description";
+                description.textContent = item.properties.opening_crawl;
+
+                card.appendChild(title);
+                card.appendChild(description);
+                gallery.appendChild(card);
+            });
+        }
+
+        else if (peopleButton.classList.contains("active")) {
+
+            results = data.results;
+            
+
+            results.forEach(async item => {
+                temp_data = await fetch(item.url);
+                temp = await temp_data.json();
+                filtered = temp.result.properties;
+
+                const card = document.createElement("div");
+                card.className = "card";
+
+                const title = document.createElement("h3");
+                title.className = "title";
+                title.textContent = filtered.name;
+
+                const description = document.createElement("p");
+                description.className = "description";
+                description.textContent = filtered.starship_class;
+
+                card.appendChild(title);
+                card.appendChild(description);
+                gallery.appendChild(card);
+            });
+        }
         
 
     } catch(err) {
@@ -40,4 +80,14 @@ const fetchData = async () => {
 
 }
 
-fetchData();
+
+
+filmsButton.addEventListener("click", () => {
+    setActive(filmsButton);
+    fetchData("https://www.swapi.tech/api/films");
+})
+
+peopleButton.addEventListener("click", () => {
+    setActive(peopleButton);
+    fetchData("https://www.swapi.tech/api/starships");
+})
